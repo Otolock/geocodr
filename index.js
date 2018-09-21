01cd2912bfd2f34b80394277ea95423fc418e869
Antonio Santos <aj.santos128@gmail.com>
2018-09-20 23:33:32 -0400

    Geocoder was refactored and is now an object.
    Geocoder is now an object that takes a Google Geocoding API key as an
    argument and has a function geocoder that returns the latitude and
    longitude for a given address.

--- index.js
+++ index.js
@@ -1,42 +1,36 @@
 var request = require('request');
 
-/* Returns latitude and longitude for a given address */
-module.exports.getGeocode = function (API_KEY, address, callback) {
-	const options = {
-		url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address.replace(' ', '+')}&key=${API_KEY}`,
-		method: 'GET',
-		headers: {
-			'Accept': 'application/json',
-			'Accept-Charset': 'utf-8',
+function Geocoder(API_KEY) {
+	this.BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?';	
+	this.API_KEY = API_KEY;
+
+	/* Given an address, returns object containing lat and lng coordinates */
+	this.geocode = function (address, callback) {
+		const options = {
+			url: this.BASE_URL + 'address=' +  address.replace(' ', '+') + '&key=' + this.API_KEY,
+			method: 'GET',
+			headers: {
+				'Accept': 'application/json',
+				'Accept-Charset': 'utf-8',
+			}
 		}
+
+		request(options, function(err, res, body) {
+			if (err) {
+				callback(err);
+			}
+
+			let data = JSON.parse(body);
+			let coordinates = {
+				lat: data.results[0].geometry.location.lat,
+				lng: data.results[0].geometry.location.lng
+			}
+
+			callback(null, coordinates);
+		});
 	}
+};
 
-	request(options, function(err, res, body) {
-		if (err) {
-			callback(err);
-		}
-
-		let data = JSON.parse(body);
-		callback(null, data.results[0].geometry.location);
-	});
-}
-
-module.exports.getReverseGeocode = function (API_KEY, lat, lng, callback) {
-	const options = {
-		url: `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat + ',' + lng}&key=$(API_KEY)`,
-		method: 'GET',
-		headers: {
-			'Accept': 'application/json',
-			'Accept-Charset': 'utf-8',
-		}
-	}
-
-	request(options, function(err, res, body) {
-		if (err) {
-			callback(err);
-		}
-
-		let data = JSON.parse(body);
-		callback(null, data.results[0].formatted_address);
-	});
+module.exports = {
+	Geocoder: Geocoder
 }
